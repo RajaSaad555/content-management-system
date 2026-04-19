927a4e49cd43267f4f5f3b2005e96d8118ef85de
Raja Saad <rajasaadi715@gmail.com>
2026-04-19 13:00:49 +0300

    Add files via upload

---
+++ Web.UI/Views/Shared/Components/RenderMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObject.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class RenderMenuViewComponent : ViewComponent
+{
+    private readonly IPageService _pageService;
+
+    public RenderMenuViewComponent(IPageService pageService)
+    {
+        _pageService = pageService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var menu = await _pageService.GetMenuHierarchyAsync();
+        
+        return View("MenuBar", menu);
+    }
+
+}
---
+++ Web.UI/Views/Shared/Components/SliderPartialViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObject.Interface;
+using Microsoft.AspNetCore.Mvc;
+namespace Web.UI.Views.Shared.Components
+{
+    public class SliderPartialViewComponent : ViewComponent
+    {
+        private readonly ISliderService _sliderService;
+
+        public SliderPartialViewComponent(ISliderService sliderService)
+        {
+            _sliderService = sliderService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var sliderlist = await _sliderService.GetSliderListAsync();
+
+            return View("SliderPartial", sliderlist);
+        }
+    }
+}
